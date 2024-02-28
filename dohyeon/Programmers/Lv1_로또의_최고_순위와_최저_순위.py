28d23d2a96c354b00a79c872fef0382f85e703f7
pental <pental@kakao.com>
2024-02-29 01:03:21 +0900

    Update level 1 - 로또의 최고 순위와 최저 순위 for code refactoring

--- dohyeon/Programmers/Lv1_로또의_최고_순위와_최저_순위.py
+++ dohyeon/Programmers/Lv1_로또의_최고_순위와_최저_순위.py
@@ -8,12 +8,15 @@ def solution(lottos, win_nums):
     for i in lottos : 
         if i == 0 :
             zero_count += 1
-    if min_rank == 6 : answer[1] = 1
-    elif min_rank == 5: answer[1] = 2
-    elif min_rank == 4 : answer[1] = 3
-    elif min_rank == 3 : answer[1] = 4
-    elif min_rank == 2 : answer[1] = 5
-    else : answer[1] = 6
+    ''' Code Refactoring '''
+    # if min_rank == 6 : answer[1] = 1
+    # elif min_rank == 5: answer[1] = 2
+    # elif min_rank == 4 : answer[1] = 3
+    # elif min_rank == 3 : answer[1] = 4
+    # elif min_rank == 2 : answer[1] = 5
+    # else : answer[1] = 6
+    rank_mapping = {6: 1, 5: 2, 4: 3, 3: 4, 2: 5}
+    answer[1] = rank_mapping.get(min_rank, 6)
     temp = (answer[1] - zero_count)
     if temp <= 0 : temp = 1
     answer[0] = temp

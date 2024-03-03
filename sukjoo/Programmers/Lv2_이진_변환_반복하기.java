632fb7c6f695fc37adbc97c15d20904a047c5a3c
Ahnsukjoo <110189683+Ahnsukjoo@users.noreply.github.com>
2024-03-03 17:46:37 +0900

    Add level 2 - 이진 변환 반복하기

---
+++ sukjoo/Programmers/Lv2_이진_변환_반복하기.java
@@ -0,0 +1,26 @@
+class Lv2_이진_변환_반복하기 {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int count0 = 0;
+        int length = 0;
+        int answer2 = 0;
+        while(s.length() > 1){
+            length = 0;
+            answer2++;
+            System.out.println(s );
+            for(int i = 0 ; i < s.length(); i++){
+                if(s.charAt(i)=='0'){
+                    count0++;
+                    
+                }
+                else{
+                    length++;
+                }
+            }
+            s = Integer.toBinaryString(length);
+        }
+        answer[1] = count0;
+        answer[0] = answer2;
+        return answer;
+    }
+}

d7d9b10024c0b5a42fbbfb9613f936fee506c632
conycomy <imgyeong4936@gmail.com>
2024-03-09 23:50:15 +0900

    Add 문자열 붙여서 출력하기.java

---
+++ imgyeong/Programmers/Level_0/문자열 붙여서 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class 문자열 붙여서 출력하기{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        System.out.print(a+b);
+    }
+         {
+    
+}
+}

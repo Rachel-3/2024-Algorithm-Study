c70bde05b6bdf33b84ac23a13f18b54cd8f0281b
seongwonjung <122601269+seongwonjung@users.noreply.github.com>
2024-03-31 19:20:52 +0900

    Add level 2 - 멀리 뛰기

---
+++ seongwon/Programmers/Lv2_멀리_뛰기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    int answer = 0;
+    vector<int> arr;
+    arr.push_back(1);
+    arr.push_back(1);
+    
+    for(int i = 2; i <= n; i++){
+        arr.push_back((arr[i-1]+arr[i-2])%1234567);
+    }
+    
+    answer = arr[n];
+    return answer;
+}

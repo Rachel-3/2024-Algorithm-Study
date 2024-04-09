14473243e7946964a61bf324b7c0c8ac057192be
pental <pental@kakao.com>
2024-04-09 11:07:52 +0900

    Add level 2 - 뒤에 있는 큰수 찾기

---
+++ dohyeon/Programmers/Level_2/Lv2_뒤에_있는_큰수_찾기.py
@@ -0,0 +1,28 @@
+def solution(numbers):
+    stack = []
+    answer = [-1] * len(numbers)
+    
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+        stack.append(i)
+    
+    return answer
+
+
+''' 시간 초과 '''
+# def solution(numbers):
+#     answer = []
+#     for i in range(len(numbers)) :
+#         temp = numbers[i:]
+#         number_max = max(temp)
+#         check = 0
+#         if number_max > numbers[i] :
+#             for j in temp :
+#                 if numbers[i] < j :
+#                     answer.append(j)
+#                     break
+#         else :
+#             answer.append(-1)
+#     return answer
+''' 시간초과 나서 단일 O(n)으로 바꾸기 위해서 스택 사용 '''

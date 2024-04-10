5fab6f62abb2cb712a5f3833c455a8be043dee6a
pental <pental@kakao.com>
2024-04-10 09:02:18 +0900

    Add level 2 - 뒤에 있는 큰수 찾기

--- dohyeon/Programmers/Level_2/Lv2_뒤에_있는_큰수_찾기.py
+++ dohyeon/Programmers/Level_2/Lv2_뒤에_있는_큰수_찾기.py
@@ -1,12 +1,10 @@
 def solution(numbers):
     stack = []
     answer = [-1] * len(numbers)
-    
     for i in range(len(numbers)):
         while stack and numbers[stack[-1]] < numbers[i]:
             answer[stack.pop()] = numbers[i]
         stack.append(i)
-    
     return answer
 
 

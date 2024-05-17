d4473ecefc9a25de0057aa4b845954d8c7e14bf1
Rachel <cofla226@naver.com>
2024-05-18 04:29:30 +0900

    Add level 0 - 배열 만들기 4

---
+++ chaerim/Programmers/Level_0/Lv0_배열_만들기_4.py
@@ -0,0 +1,15 @@
+def solution(arr):
+    stk = []
+
+    i = 0
+    while i < len(arr):
+        if not stk:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] >= arr[i]:
+            stk.pop()
+
+    return stk

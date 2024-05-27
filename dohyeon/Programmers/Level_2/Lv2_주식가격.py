7f4faa8d211bc08eef0e7cf556f019e0ba46ffc2
pental <pental@kakao.com>
2024-05-27 11:03:04 +0900

    Add level 2 - 주식가격

--- dohyeon/Programmers/Level_2/Lv2_주식가격.py
+++ dohyeon/Programmers/Level_2/Lv2_주식가격.py
@@ -2,9 +2,11 @@ def solution(prices):
     answer = []
     for i in range(len(prices)) :
         temp = 0
-        for j in range(i, len(prices)) :
-            
+        for j in range(i, len(prices)) : 
             if prices[i] <= prices[j] :
                 temp += 1
+            else :
+                temp += 1
+                break
         answer.append(temp - 1)
     return answer

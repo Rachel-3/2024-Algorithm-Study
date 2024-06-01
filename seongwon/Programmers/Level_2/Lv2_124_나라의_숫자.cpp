d98785e5f34b1883adb0af236bcff9c87eca149f
seongwonjung <122601269+seongwonjung@users.noreply.github.com>
2024-06-01 16:49:15 +0900

    Add level 2 - 124 나라의 숫자

---
+++ seongwon/Programmers/Level_2/Lv2_124_나라의_숫자.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    int share = n;
+    int remainder = -1;
+    
+    while (share!=0) {
+        remainder = share % 3;
+        share = share / 3;
+
+        if (remainder == 0) { //나누어 떨어질 경우 몫을 1빼줌
+            answer = "4" + answer;
+            share--;
+        }
+        else if (remainder == 1) {
+            answer = "1" + answer;
+        }
+        else if (remainder == 2) {
+            answer = "2" + answer;
+        }
+    }
+    return answer;
+}

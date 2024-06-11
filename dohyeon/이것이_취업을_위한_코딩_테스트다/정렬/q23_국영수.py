c12df220233028e3138a6af285579440febc2921
pental <pental@kakao.com>
2024-06-11 16:44:27 +0900

    Update 이것이 취업을 위한 코딩 테스트다 - 정렬 - 국영수

--- dohyeon/이것이_취업을_위한_코딩_테스트다/정렬/q23_국영수.py
+++ dohyeon/이것이_취업을_위한_코딩_테스트다/정렬/q23_국영수.py
@@ -8,3 +8,34 @@ users.sort(key = lambda x : (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for i in users :
     print(i[0])
+
+'''
+input )
+12
+Ju 50 60 100
+Sa 80 60 50
+Su 80 70 100
+So 50 60 90
+Ha 50 60 100
+Ka 60 80 100
+Do 80 60 100
+Se 70 70 70
+Wo 70 70 90 
+Sanghyun 70 70 80
+nsj 80 80 80
+tae 50 60 90
+
+Output )
+Do
+Sa
+Su
+nsj
+Wo
+Sanghyun
+Se
+Ka
+Ha
+Ju
+So
+tae
+'''

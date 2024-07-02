a9b5a90347d8ff770020d728a446a67faea36507
pental <pental@kakao.com>
2024-07-03 08:28:30 +0900

    Add level 2 - 두 큐 합 같게 만들기

---
+++ dohyeon/Programmers/Level_2/Lv2_두_큐_합_같게_만들기.py
@@ -0,0 +1,32 @@
+''' 시간초과 70 / 100'''
+from collections import deque
+
+def solution(queue1, queue2):
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    sum_q1 = sum(queue1)
+    sum_q2 = sum(queue2)
+
+    for i in range(300000) :
+        if sum_q1 == sum_q2 :
+            return i
+
+        if sum_q1 > sum_q2 :
+            num = queue1.popleft()
+            queue2.append(num)
+            sum_q1 -= num
+            sum_q2 += num
+
+        elif sum(queue1) < sum(queue2)  :
+            num = queue2.popleft()
+            queue1.append(num)
+            sum_q1 += num
+            sum_q2 -= num
+    return -1
+
+
+
+print(solution([3, 2, 7, 2], [4, 6, 5, 1]))
+print(solution([1, 2, 1, 2], [1, 10, 1, 2]))
+print(solution([1, 1], [1, 5]))
+

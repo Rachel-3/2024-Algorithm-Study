c625fe542922917fbdfca7ba17c1fc7544f5fa58
Rachel <cofla226@naver.com>
2024-08-12 05:57:22 +0900

    Update level 0 - 문자열 계산하기

--- chaerim/Programmers/Level_0/Lv0_문자열_계산하기.py
+++ chaerim/Programmers/Level_0/Lv0_문자열_계산하기.py
@@ -1,17 +1,22 @@
-def solution(s):
+def solution(my_string):
     answer = 0
     stack = []
 
-    string_list = s.split()
+    string_list = my_string.split()
     
     for i in string_list:
-        if i == 'Z':
-            if stack:
-                last_num = stack.pop()
-                answer -= last_num
+        if i == '+' or i == '-':
+            stack.append(i)
         else:
             number = int(i)
-            stack.append(number)
-            answer += number
+
+            if stack and stack[-1] == '+':
+                answer += number
+                stack.pop()
+            elif stack and stack[-1] == '-':
+                answer -= number
+                stack.pop()
+            else:
+                answer += number
 
     return answer

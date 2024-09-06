ab8b85d7acc11728d85c11cd8a6140850dd327f4
Rachel <cofla226@naver.com>
2024-09-07 02:25:25 +0900

    Add level 0 - 순서쌍의 개수

---
+++ chaerim/Programmers/Level_0/Lv0_순서쌍의_개수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    answer = 0
+
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            b = n // i
+            answer += 1
+            if i != b:
+                answer += 1
+
+    return answer

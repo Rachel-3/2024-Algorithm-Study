ddb990bfdfd1e1fc846a6b8fd5b6f19e7522f7a9
Rachel <cofla226@naver.com>
2024-11-14 06:08:14 +0900

    Add Level 0 - [PCCE 기출문제] 8번 / 닉네임 규칙

---
+++ chaerim/Programmers/Level_0/Lv0_PCCE_기출문제_8번_닉네임_규칙.py
@@ -0,0 +1,18 @@
+def solution(nickname):
+    answer = ""
+    for letter in nickname:
+        if letter == "l":
+            answer += "I"
+        elif letter == "w":
+            answer += "vv"
+        elif letter == "W":
+            answer += "VV"
+        elif letter == "O":
+            answer += "0"
+        else:
+            answer += letter
+    while len(answer) < 4:
+        answer += "o"
+    if len(answer) > 8:
+        answer = answer[:8]
+    return answer

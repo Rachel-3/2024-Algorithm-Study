42e23d96b5fe82d28abc489f10f94fa102fd5db0
Rachel <cofla226@naver.com>
2025-01-12 23:21:26 +0900

    Add Baekjoon 9498 - 시험 성적

---
+++ chaerim/Baekjoon/Bronze/9498_Bronze_5_시험_성적.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if 90 <= a <= 100:
+    print("A")
+elif 80 <= a < 90:
+    print("B")
+elif 70 <= a < 80:
+    print("C")
+elif 60 <= a < 70:
+    print("D")
+else:
+    print("F")
